a2ef82a9092371df8a84b10ab334724b2238d0bd
sghctoma <sghctoma@gmail.com>
2018-05-01 07:47:46 +0200

    Added DummySwitch, and modified sample config to use it.

--- TerraPi/devices/dummy.py
+++ TerraPi/devices/dummy.py
@@ -1,13 +1,14 @@
+import logging
 import random
 
 from ..db import SensorType, Sensor
-from .device import SensorDevice
+from .device import SensorDevice, ControllerDevice
 
 
 class RandomTemperatureHumidity(SensorDevice):
     """
     Sensor device that returns random temperature and humidity measurements.
-    For testing purposes only!.
+    For testing purposes only!
     """
     def __init__(self, app, config):
         super().__init__(app, config, [
@@ -19,3 +20,21 @@ class RandomTemperatureHumidity(SensorDevice):
             (SensorType.temperature, random.randint(18, 50)),
 	    (SensorType.humidity, random.randint(30, 90)),
 	]
+
+class DummySwitch(ControllerDevice):
+    """
+    A switch that switches nothing, only logs status changes.
+    For testing purposes only!
+    """
+    def __init__(self, app, config):
+        super().__init__(app, config)
+
+    def control(self, value):
+        if value == 'on':
+            logging.info("Switching {} on.".format(self.name))
+        elif value == 'off':
+            logging.info("Switching {} off.".format(self.name))
+        else:
+            logging.warn("{} received invalid control value ({}).".format(
+                self.name, value))
+
